9710416f4a6cf3135db324fc9717eb07287a5b7d
tituskings <kingtitus300@gmail.com>
2025-02-20 16:41:28 +0100

    form validation

--- lib/addBucketList.dart
+++ lib/addBucketList.dart
@@ -36,41 +36,81 @@ class _AddBucketListScreenState extends State<AddBucketListScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var addForm = GlobalKey<FormState>();
+
     return Scaffold(
         appBar: AppBar(
           title: Text("Add Bucket List"),
         ),
-        body: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(label: Text("Item")),
-              controller: itemText,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            TextField(
-              decoration: InputDecoration(label: Text("Estimated Cost")),
-              controller: costText,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            TextField(
-              decoration: InputDecoration(label: Text("Image url")),
-              controller: imageUrlText,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: [
-                Expanded(
-                    child: ElevatedButton(
-                        onPressed: addData, child: Text("Add Item"))),
-              ],
-            ),
-          ],
+        body: Form(
+          key: addForm,
+          child: Column(
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value.toString().length < 3) {
+                    return "Must be more than 3 characters";
+                  }
+                  if (value == null || value.isEmpty) {
+                    return "This must not be empty";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(label: Text("Item")),
+                controller: itemText,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value.toString().length < 3) {
+                    return "Must be more than 3 characters";
+                  }
+                  if (value == null || value.isEmpty) {
+                    return "This must not be empty";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(label: Text("Estimated Cost")),
+                controller: costText,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value.toString().length < 3) {
+                    return "Must be more than 3 characters";
+                  }
+                  if (value == null || value.isEmpty) {
+                    return "This must not be empty";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(label: Text("Image url")),
+                controller: imageUrlText,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: ElevatedButton(
+                          onPressed: () {
+                            if (addForm.currentState!.validate()) {
+                              addData();
+                            }
+                          },
+                          child: Text("Add Item"))),
+                ],
+              ),
+            ],
+          ),
         ));
   }
 }
